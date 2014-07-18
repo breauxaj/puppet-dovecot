12ee2f4f1b12cfc57cdd2309738bebd9b15ca5b0
Adam Breaux <breauxaj@gmail.com>
2014-07-18 11:02:40 -0400

    added docs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,31 @@
+# Class: dovecot
+#
+# This class installs the dovecot server
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the packages
+#
+# Actions:
+#   - Installs the dovecot package
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'dovecot':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'dovecot':
+#      ensure => 'absent'
+#    }
+#
 class dovecot (
   $ensure = 'latest'
 ){
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,3 +1,29 @@
+# Define: dovecot::service
+#
+# This define manages the Dovecot service
+#
+# Parameters:
+#
+#  ensure:
+#    Controls the state of the service
+#
+#  enable:
+#    Controls the service start on boot
+#
+# Actions:
+#   - Stops/starts the Dovecot service
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  To enable the service, use:
+#
+#    dovecot::service { 'default':
+#      ensure => running,
+#      enable => true
+#    }
+#
 define dovecot::service (
   $ensure,
   $enable
